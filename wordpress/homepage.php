aeb6f12642ec585477a4415fd035cd0c34c51380
Kathleen Sullivan <kathleensulli@gmail.com>
2013-08-15 22:32:42 +0200

    adding wordpress start

---
+++ wordpress/homepage.php
@@ -0,0 +1,121 @@
+<html>
+
+  <head>
+	<style>
+	 body {margin:0px;}
+	</style>
+	
+	
+	<link href="helio.css" media="" rel="stylesheet" type="text/css" />
+  </head>
+	
+	
+	 <body>
+		
+		<div id="helio-header">
+		
+		   <div id="helio-header-banner">
+		       <div id="helio-logo"></div>
+		       <div id="helio-motto">Hydro, Eolien, Light, Insulation, Organomass</div>
+		       <div id="helio-search"></div>
+		       <div id="helio-social"></div>
+		   </div>
+		
+		   <div id="helio-navigation">
+		   	    <ul>
+				  <li class="extraspace"><a href="">About</a></li>
+				  <li><a href="">Focus</a></li>
+				  <li><a href="">Projects</a></li>
+				  <li><a href="">Publications</a></li>
+				  <li><a href="">Partners &amp; Events</a></li>
+				  <li class="extraspace"><a href="">News</a></li>
+					
+				</ul>
+			
+		  </div><!--END  NAV -->
+			
+			
+		</div><!--END HEADER -->
+		
+		
+		<div id="helio-main">	
+			<div class="helio-featured">
+		        <div class="art"><img src="http://www.helio-international.org/wordpress/images/solar-energy.jpg" width="500" height="300" /></div>
+				<div class="text"><h1>Lorem ipsum dolor sit amet, consectetuer adipiscing elit,</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+		
+		
+		    </div><!--END FEATURED-->
+		
+		
+			<div class="helio-bubbles">
+				<ul>
+					<li class="bubble"><a href=""></a></li>
+					<li class="bubble"><a href=""></a></li>
+					<li class="bubble"><a href=""></a></li>
+					<li class="bubble"><a href=""></a></li>
+					<li class="bubble"><a href=""></a></li>
+				
+				
+			</div><!--END BUBBLES-->
+			
+			
+			
+			<div class="helio-map">
+				<h1>Explore HELIO projects</h1>
+				
+				
+			</div><!--END MAP-->
+			
+			
+			
+			<div class="helio-action"></div><!--END GET INVOLVED-->
+			
+			<div class="helio-events"></div><!--END EVENTS-->
+			<div class="helio-pubs"></div><!--END PUBLICATION HIGHLIGHT-->
+			
+		
+		
+		</div><!--END MAIN-->
+		
+		<div id="helio-footer">
+			<div id="helio-social-footer"></div>
+			<div id="helio-newsletter"></div>
+			<div id="helio-contact-footer"></div>
+			
+			
+			<div id="helio-sitemap"></div>
+		</div><!--END FOOTER -->
+		
+		<div id="helio-bottom"></div>
+		  
+		   	
+			
+			
+		
+		
+		
+		
+		
+	 
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	</body>
+	
+</html>
